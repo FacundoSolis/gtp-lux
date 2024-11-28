9c03d0505c375d3628e1174ef7d43b9ddbd79fee
facundo  solis <fakun@facundos-MacBook-Pro.local>
2024-11-28 17:10:49 +0100

    Initial commit

--- js/app.js
+++ js/app.js
@@ -7,3 +7,20 @@ window.addEventListener('scroll', function() {
     });
   });
   
+  // JavaScript para funcionalidad del slider
+document.querySelectorAll('.slider-with-arrows').forEach((container) => {
+  const slides = container.querySelector('.slides');
+  const images = slides.querySelectorAll('img');
+  const totalImages = images.length;
+  let index = 0;
+
+  container.querySelector('.prev').addEventListener('click', () => {
+    index = (index - 1 + totalImages) % totalImages;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+  });
+
+  container.querySelector('.next').addEventListener('click', () => {
+    index = (index + 1) % totalImages;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+  });
+});
