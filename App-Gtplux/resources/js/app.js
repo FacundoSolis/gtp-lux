240fb9104ce49c24770b2405385c1b42306719d6
facundo  solis <fakun@facundos-MacBook-Pro.local>
2024-12-09 09:35:04 +0100

    app-gtplux

---
+++ App-Gtplux/resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Example from './components/Example';
+
+const container = document.getElementById('app');
+if (container) {
+    const root = createRoot(container);
+    root.render(<Example />);
+}
