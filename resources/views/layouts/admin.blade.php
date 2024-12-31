53fc84d8fbb48d2b7ed7b7741c4031e68430d24c
facundo  solis <fakun@facundos-MacBook-Pro.local>
2024-12-31 16:05:27 +0100

    g

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>@yield('title', 'Panel de Administración')</title>
     <!-- CSS de Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
