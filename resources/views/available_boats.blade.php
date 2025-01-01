8fb013d9e1eb797cafd8f67ec6084f686ad25700
facundo  solis <fakun@facundos-MacBook-Pro.local>
2025-01-01 13:56:57 +0100

    a

--- resources/views/available_boats.blade.php
+++ resources/views/available_boats.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.public')
 
 @push('styles')
-@vite('resources/css/menu.css')
-@vite('resources/css/available-boats.css')
+<link rel="stylesheet" href="{{ asset('build/assets/menu-BnIop0I-.css') }}">
+<link rel="stylesheet" href="{{ asset('build/assets/available-boats-BIl8H1si.css') }}">
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 @endpush
@@ -140,7 +140,7 @@
 @endsection
 
 @section('scripts')
-@vite('resources/js/menu.js')
+<script src="{{ asset('build/assets/menu-Cd3QX7BG.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script>
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -28,7 +28,7 @@
     <main class="content">
         @yield('content')
     </main>
-    </div>
+    
 
     <!-- Scripts globales -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
